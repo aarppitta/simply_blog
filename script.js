e9669e2fc520b9ff357fc8dbfb1b524f49212133
Arpita Chaudhari <arpitachaudhari26@gmail.com>
2023-09-22 19:47:39 +0100

    simply blog

---
+++ script.js
@@ -0,0 +1,19 @@
+
+const blog_title = document.querySelector('#title');
+const blog_content = document.querySelector('#content');
+const blog_img = document.querySelector('#img');
+const blog_btn = document.querySelector('#btn');
+const blog_heading = document.querySelector('.blog-heading');
+
+blog_btn.addEventListener('click', (e) => {
+  addValue();
+});
+
+const addValue = () => {
+  var title = blog_title.value;
+  
+}
+
+
+
+
